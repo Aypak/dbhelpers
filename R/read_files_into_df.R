e9278d7879cf76baee1e98b5bc93605c11e95ff1
Kapya <kapsakala@gmail.com>
2020-07-06 15:24:54 +0200

    added  read_files_into_df function

---
+++ R/read_files_into_df.R
@@ -0,0 +1,29 @@
+#' Function to read files from a directory and bind them into a single \code{data.frame}
+#'
+#' @param file_directory Name or path to the directory containing files of interest
+#' @param file_pattern Regular expression to match the file names on. Default is "*.csv"
+#'
+#' @return A \code{data.frame} containing rows from the files in the directory.
+#' @export
+#'
+read_files_into_df <- function(file_directory, file_pattern="*.csv"){
+  # Create a placeholder data frame
+  placeholder_df <- data.frame()
+
+  # Get a vector of all of the file names in a directory
+  filenames <- list.files(
+    file_directory,
+    pattern = file_pattern,
+    full.names = T)
+
+  # Loop through the list of files
+  for(i in 1:length(filenames)){
+    # Read each file and bind it to the placeholder df
+    file <- readr::read_csv(filenames[i])
+    placeholder_df <- placeholder_df %>% plyr::rbind.fill(file)
+  }
+
+  # Return placeholder df
+  return(placeholder_df)
+
+}
