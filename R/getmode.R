ec3dc514f93f6454c1bca2f51cd53cb919b89d09
Kapya <kapsakala@gmail.com>
2020-08-27 10:47:16 +0200

    function to get mode from a vector. R currently has no built-in function to get mode

---
+++ R/getmode.R
@@ -0,0 +1,18 @@
+#' Get the mode from a vector
+#'
+#' @param v A vector
+#'
+#' @return A vector containing the most frequent value in a vector.
+#' If there is a tie, the top values will be returned.
+#' R currently has no built-in function to get mode
+#'
+#' @export
+#'
+#' @examples
+#' hps <- mtcars$hp
+#' getmode(hps)
+#'
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
