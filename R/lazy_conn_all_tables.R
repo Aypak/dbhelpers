16bcd07a09ada9eab7f4777a8f1885955cd31442
Kapya <kapsakala@gmail.com>
2020-09-30 17:24:33 +0200

    updated documentation

--- R/lazy_conn_all_tables.R
+++ R/lazy_conn_all_tables.R
@@ -4,9 +4,9 @@
 #' @param pool_conn A pool connection to a PostgreSQL database
 #' @param schemaname The schema from which to get the tables. Default is public
 #' @param prefix String to prefix the variables with
-#' @param env The environment in which to create the variables. Default is global
+#' @param env The environment in which to create the variables. Default is global environment
 #'
-#' @return Variables for lazy connections to each of the tables in the global environment
+#' @return Variables for lazy connections to each of the tables in the environment specified
 #'
 #' @export
 #'
@@ -22,13 +22,11 @@ lazy_conn_all_tables <- function(pool_conn,schemaname = "public", prefix = "", e
     dplyr::pull(table_name)
 
   # Walk through the vector above and apply the create_lazy_conn method to each table
-  # Create variable in the global environment
+  # Create variable in the environment specified
   # Add the optional prefix to each variable name
   purrr::walk(table_names,
        ~assign(
          envir = env,
          x = paste0(prefix,.),
-         value = create_lazy_conn(pool_conn,schemaname,.)
-         )
-       )
+         value = create_lazy_conn(pool_conn,schemaname,.)))
 }
