328c590daca4ae482d0d30eefe946bcf322397c8
Kapya Sakala <kapsakala@gmail.com>
2021-04-30 09:28:09 +0200

    use paste instead of str_c

--- R/str_to_uuid.R
+++ R/str_to_uuid.R
@@ -8,9 +8,7 @@
 #' @examples
 #' str_to_uuid("f3f6bf4b9c424b6fbd90bece1418a415")
 #' str_to_uuid("3a126f041a9e4127978bb6544a099d0d")
-#'
-#' @import stringr
-#'
-str_to_uuid <- function(input_string){
-  return (str_c(str_sub(input_string, 1,8), str_sub(input_string, 9,12), str_sub(input_string, 13,16), str_sub(input_string, 17,20), str_sub(input_string, 21,32), sep = "-"))
+str_to_uuid <- function(input_string) {
+  uuid_string <- paste(stringr::str_sub(input_string, 1, 8), stringr::str_sub(input_string, 9, 12), stringr::str_sub(input_string, 13, 16), stringr::str_sub(input_string, 17, 20), stringr::str_sub(input_string, 21, 32), sep = "-")
+  return(uuid_string)
 }
