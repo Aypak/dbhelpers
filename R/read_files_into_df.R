d6fe570907950dfb7c05ca1c36aa7e1803f18bef
Kapya Sakala <kapsakala@gmail.com>
2021-04-30 09:29:48 +0200

    improved linting

--- R/read_files_into_df.R
+++ R/read_files_into_df.R
@@ -2,12 +2,12 @@
 #'
 #' @param file_directory Name or path to the directory containing files of interest
 #' @param file_pattern Regular expression to match the file names on. Default is "*.csv"
-#' @param ... Arbitrary args passed to readr
+#' @param ... Arbitrary args passed to \code{fread}
 #'
 #' @return A \code{data.frame} containing rows from the files in the directory.
 #' @export
 #'
-read_files_into_df <- function(file_directory, file_pattern = "*.csv",...) {
+read_files_into_df <- function(file_directory, file_pattern = "*.csv", ...) {
   # Create a placeholder data frame
   placeholder_df <- data.frame()
 
@@ -21,7 +21,7 @@ read_files_into_df <- function(file_directory, file_pattern = "*.csv",...) {
   # Loop through the list of files
   for (i in 1:length(filenames)) {
     # Read each file and bind it to the placeholder df
-    file <- data.table::fread(file = filenames[i],...)
+    file <- data.table::fread(file = filenames[i], ...)
     placeholder_df <- placeholder_df %>% plyr::rbind.fill(file)
   }
 
