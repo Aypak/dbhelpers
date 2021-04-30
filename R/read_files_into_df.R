e41f1834cc2790f0ef1d496954f1e4bb6c849ff1
Kapya Sakala <kapsakala@gmail.com>
2021-04-30 08:58:06 +0200

    use fread instead of readr. which cut execution time by 75%

--- R/read_files_into_df.R
+++ R/read_files_into_df.R
@@ -2,6 +2,7 @@
 #'
 #' @param file_directory Name or path to the directory containing files of interest
 #' @param file_pattern Regular expression to match the file names on. Default is "*.csv"
+#' @param ... Arbitrary args passed to readr
 #'
 #' @return A \code{data.frame} containing rows from the files in the directory.
 #' @export
@@ -20,7 +21,7 @@ read_files_into_df <- function(file_directory, file_pattern = "*.csv",...) {
   # Loop through the list of files
   for (i in 1:length(filenames)) {
     # Read each file and bind it to the placeholder df
-    file <- readr::read_csv(file = filenames[i],...)
+    file <- data.table::fread(file = filenames[i],...)
     placeholder_df <- placeholder_df %>% plyr::rbind.fill(file)
   }
 
