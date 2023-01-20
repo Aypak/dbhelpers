51b0b61492734326355fb413a297c6934fda6fbb
Kapya Sakala <kapsakala@gmail.com>
2023-01-20 13:52:40 +0200

    check if string has 32 characters before converting to uuid

--- R/str_to_uuid.R
+++ R/str_to_uuid.R
@@ -9,6 +9,15 @@
 #' str_to_uuid("f3f6bf4b9c424b6fbd90bece1418a415")
 #' str_to_uuid("3a126f041a9e4127978bb6544a099d0d")
 str_to_uuid <- Vectorize(function(input_string) {
-  uuid_string <- paste(stringr::str_sub(input_string, 1, 8), stringr::str_sub(input_string, 9, 12), stringr::str_sub(input_string, 13, 16), stringr::str_sub(input_string, 17, 20), stringr::str_sub(input_string, 21, 32), sep = "-")
+  if (nchar(input_string) != 32) {
+    stop("Input string must have 32 characters")
+  }
+
+  uuid_string <- paste(stringr::str_sub(input_string, 1, 8),
+                       stringr::str_sub(input_string, 9, 12),
+                       stringr::str_sub(input_string, 13, 16),
+                       stringr::str_sub(input_string, 17, 20),
+                       stringr::str_sub(input_string, 21, 32),
+                       sep = "-")
   return(uuid_string)
 })
