9f2838b5a01dd07b379a49039f8b1662421af369
Kapya Sakala <kapsakala@gmail.com>
2023-01-20 14:42:38 +0200

    added try-catch to file read loop. convert duplicate columns of different types to character type. print out name of file and error if encountered

--- R/read_files_into_df.R
+++ R/read_files_into_df.R
@@ -8,28 +8,38 @@
 #' @export
 #'
 read_files_into_df <- function(file_directory, file_pattern = "*.csv", ...) {
-  # Create a placeholder data frame
-  # placeholder_df <- data.frame()
-
   # Get a vector of all of the file names in a directory
-  filenames <- list.files(
-    file_directory,
-    pattern = file_pattern,
-    full.names = T
-  )
+  filenames <- list.files(file_directory, pattern = file_pattern, full.names = T)
 
-  # Loop through the list of files
-  # for (i in 1:length(filenames)) {
-  #   # Read each file and bind it to the placeholder df
-  #   file <- data.table::fread(file = filenames[i], ...)
-  #   placeholder_df <- placeholder_df %>% plyr::rbind.fill(file)
-  # }
+  # Create a placeholder data frame
+  placeholder_df <- data.frame()
 
-  file_list <- purrr::map(filenames, data.table::fread, ...)
+  # Loop through the list of filenames
+  for (i in 1:length(filenames)) {
+    # Try to read each file
+    tryCatch(
+      {
+        file <- data.table::fread(file = filenames[i], ...)
+        # Check for duplicate columns and update the data type to character
+        if (nrow(placeholder_df) > 0) {
+          duplicate_cols <- intersect(colnames(file), colnames(placeholder_df))
+          for (col in duplicate_cols) {
+            if (class(file[[col]]) != class(placeholder_df[[col]])) {
+              placeholder_df[[col]] <- as.character(placeholder_df[[col]])
+              file[[col]] <- as.character(file[[col]])
+            }
+          }
+        }
+        # Bind the file read to the placeholder df, fill any missing columns with null
+        placeholder_df <- plyr::rbind.fill(placeholder_df, file)
+      },
+      # If there is an error reading a file, print out the name of the file and the error
+      error = function(e) {
+        print(paste("An error occurred while reading file", filenames[i], ":", e))
+      }
+    )
+  }
 
-  files_bound <- plyr::rbind.fill(file_list)
-
-  # Return placeholder df
-  # return(placeholder_df)
-  return(files_bound)
+  # Return the completely bound df
+  return(placeholder_df)
 }
