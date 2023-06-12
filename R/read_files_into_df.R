16cfc97aca2f773e2fa565368da0e727188d54ad
Kapya Sakala <kapsakala@gmail.com>
2023-06-12 15:46:50 +0200

    added functionality to read excel files

--- R/read_files_into_df.R
+++ R/read_files_into_df.R
@@ -1,45 +1,71 @@
 #' Read files from a directory and bind them into a single \code{data.frame}
 #'
-#' @param file_directory Name or path to the directory containing files of interest
-#' @param file_pattern Regular expression to match the file names on. Default is "*.csv"
+#' @param file_directory Path to the directory containing files of interest
+#' @param file_pattern File pattern or regular expression to match the file names on. Supported file types are csv, xlsx, and xls
+#' @param files_recursive Boolean. Search for files recursively in the current directory or not. Default is FALSE
+#' @param clean_names Boolean. Use \code{janitor::clean_names()} on the data as it is being read. Default is FALSE
 #' @param ... Arbitrary args passed to \code{fread}
 #'
 #' @return A \code{data.frame} containing rows from the files in the directory.
 #' @export
 #'
-read_files_into_df <- function(file_directory, file_pattern = "*.csv", ...) {
-  # Get a vector of all of the file names in a directory
-  filenames <- list.files(file_directory, pattern = file_pattern, full.names = T)
+read_files_into_df <- function(file_directory, file_pattern = rebus::or("*.csv", "*.xlsx", "*.xls"), files_recursive = FALSE, clean_names = FALSE, ...) {
+  # get all the files in the directory
+  filenames <- list.files(file_directory, pattern = file_pattern, full.names = TRUE, recursive = files_recursive)
 
-  # Create a placeholder data frame
-  placeholder_df <- data.frame()
+  # create an empty dataframe to hold the combined data
+  combined_data <- data.frame()
 
-  # Loop through the list of filenames
+  # loop through each file and read it into a dataframe
   for (i in 1:length(filenames)) {
-    # Try to read each file
+    # try to read the file as csv or excel
     tryCatch(
       {
-        file <- data.table::fread(file = filenames[i], ...)
+        # read the file as csv if it matches the csv pattern
+        if (grepl(".csv", filenames[i])) {
+          file_data <- data.table::fread(file = filenames[i], ...)
+        }
+        # read the file as excel if it matches the excel pattern
+        else if (grepl(".xlsx|.xls", filenames[i])) {
+          file_data <- readxl::read_excel(filenames[i], ...)
+        }
+        # throw an error if the file does not match either pattern
+        else {
+          stop(paste("File type not supported:", filenames[i]))
+        }
+
         # Check for duplicate columns and update the data type to character
-        if (nrow(placeholder_df) > 0) {
-          duplicate_cols <- intersect(colnames(file), colnames(placeholder_df))
+        if (nrow(combined_data) > 0) {
+          duplicate_cols <- intersect(colnames(file), colnames(combined_data))
           for (col in duplicate_cols) {
-            if (class(file[[col]]) != class(placeholder_df[[col]])) {
-              placeholder_df[[col]] <- as.character(placeholder_df[[col]])
+            if (class(file[[col]]) != class(combined_data[[col]])) {
+              combined_data[[col]] <- as.character(combined_data[[col]])
               file[[col]] <- as.character(file[[col]])
             }
           }
         }
-        # Bind the file read to the placeholder df, fill any missing columns with null
-        placeholder_df <- plyr::rbind.fill(placeholder_df, file)
+
+        if (isTRUE(clean_names)) {
+          file_data <- file_data %>% janitor::clean_names()
+        }
+
+        combined_data <- plyr::rbind.fill(combined_data, file_data)
       },
-      # If there is an error reading a file, print out the name of the file and the error
       error = function(e) {
+        # print an error message if the file cannot be read
         print(paste("An error occurred while reading file", filenames[i], ":", e))
       }
     )
   }
 
-  # Return the completely bound df
-  return(placeholder_df)
+  # fill any uncommon columns with NA
+  all_columns <- unique(names(combined_data))
+  for (col in all_columns) {
+    if (!(col %in% names(combined_data))) {
+      combined_data[[col]] <- NA
+    }
+  }
+
+  # return the combined data
+  return(combined_data)
 }
